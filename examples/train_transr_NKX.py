1af834d10e01a9346563ffc85c6e470f3cd1a854
edithwuly <edithwuly@163.com>
2023-06-23 16:19:14 +0800

    predict

---
+++ examples/train_transr_NKX.py
@@ -0,0 +1,78 @@
+import openke
+from openke.config import Trainer, Tester, Predictor
+from openke.module.model import TransR
+from openke.module.loss import MarginLoss
+from openke.module.strategy import NegativeSampling
+from openke.data import TrainDataLoader, TestDataLoader
+
+# dataloader for training
+train_dataloader = TrainDataLoader(
+	in_path = "./benchmarks/NKX/",
+	nbatches = 100,
+	threads = 8,
+	sampling_mode = "normal",
+	bern_flag = 1,
+	filter_flag = 1,
+	neg_ent = 25,
+	neg_rel = 0)
+
+# dataloader for test
+test_dataloader = TestDataLoader("./benchmarks/NKX/", "link")
+
+# define the model
+transr = TransR(
+	ent_tot = train_dataloader.get_ent_tot(),
+	rel_tot = train_dataloader.get_rel_tot(),
+	dim_e = 200,
+	dim_r = 200,
+	p_norm = 1,
+	norm_flag = True)
+
+
+# define the loss function
+model = NegativeSampling(
+	model = transr,
+	loss = MarginLoss(margin = 4.0),
+	batch_size = train_dataloader.get_batch_size()
+)
+
+# train the model
+trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 1.0, use_gpu = True)
+trainer.run()
+transr.save_checkpoint('./checkpoint/transr_nkx.ckpt')
+
+# test the model
+# transe.load_checkpoint('./checkpoint/transe_nkx.ckpt')
+# tester = Tester(model = transe, data_loader = test_dataloader, use_gpu = True)
+# tester.run_link_prediction(type_constrain = False)
+
+# predict
+transr.load_checkpoint('./checkpoint/transr_nkx.ckpt')
+predictor = Predictor(model = transr, data_loader = test_dataloader, use_gpu = True)
+links = predictor.run_link_prediction(type_constrain = False)
+fout = open("nkx_transr_prediction.txt", "w", encoding="utf-8")
+entity2id = open("./benchmarks/NKX/entity2id.txt", encoding="utf-8")
+entityId = {}
+for line in entity2id.readlines():
+	line = line.strip("\n").split("\t")
+	if len(line) != 2:
+		continue
+	entityId[line[1]] = line[0]
+entity2id.close()
+relation2id = open("./benchmarks/NKX/relation2id.txt", encoding="utf-8")
+relationId = {}
+for line in relation2id.readlines():
+	line = line.strip("\n").split("\t")
+	if len(line) != 2:
+		continue
+	relationId[line[1]] = line[0]
+relation2id.close()
+for link in links:
+	link = link.split(" ")
+	if len(link) != 3:
+		continue
+	head = entityId[link[0]]
+	tail = entityId[link[1]]
+	relation = relationId[link[2]]
+	fout.write(head + "," + relation + "," + tail + "\n")
+fout.close()
