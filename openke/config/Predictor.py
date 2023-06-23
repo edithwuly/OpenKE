613b067048e317f49805f75eee466f3aed79756f
edithwuly <edithwuly@163.com>
2023-06-23 14:01:43 +0800

    predict

--- openke/config/Predictor.py
+++ openke/config/Predictor.py
@@ -80,13 +80,15 @@ class Predictor(object):
             # print(score)
             for i in range(len(score)):
                 if score[i] >= 20.0:
-                    print(data_head['batch_h'] + "," + data_head['batch_t'] + "," + data_head['batch_r'])
+                    print(str(data_head['batch_h'][i]) + "," + str(data_head['batch_t'][i]) +
+                          "," + str(data_head['batch_r'][i]))
             self.lib.testHead(score.__array_interface__["data"][0], index, type_constrain)
             score = self.test_one_step(data_tail)
             # print(score)
             for i in range(len(score)):
                 if score[i] >= 20.0:
-                    print(data_tail['batch_h'] + "," + data_tail['batch_t'] + "," + data_tail['batch_r'])
+                    print(str(data_tail['batch_h'][i]) + "," + str(data_tail['batch_t'][i]) +
+                          "," + str(data_tail['batch_r'][i]))
             self.lib.testTail(score.__array_interface__["data"][0], index, type_constrain)
         self.lib.test_link_prediction(type_constrain)
 
